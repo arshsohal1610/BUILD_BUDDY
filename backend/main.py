c69f29c8d0b5c6a63df7bb66772554e25dd12033
arshsohal1610 <arshsohal1610@gmail.com>
2026-08-07 15:14:51 -0700

    Allow Netlify frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,10 @@ app = FastAPI()
 # Allow the React frontend to communicate with this backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"http://(localhost|127\.0\.0\.1)(:\d+)?",
+    allow_origins=[
+        "https://buildbuddies.netlify.app",
+    ],
+    allow_origin_regex=r"http://(localhost|127.0.0.1)(:\d+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
